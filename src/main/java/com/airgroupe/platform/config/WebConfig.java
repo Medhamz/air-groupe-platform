a11bea7a3b3831adafdb64579225037879caeebd
Sidi Mohamed Hamza <smhamza@opessoftware.com>
2026-08-18 23:36:02 +0100

    Fixing webconfig

--- src/main/java/com/airgroupe/platform/config/WebConfig.java
+++ src/main/java/com/airgroupe/platform/config/WebConfig.java
@@ -4,12 +4,19 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Obtenir le chemin absolu vers le dossier racine 'uploads'
+        Path uploadDir = Paths.get("uploads").toAbsolutePath();
+        String uploadPath = uploadDir.toUri().toString();
+
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations(uploadPath);
     }
 }
