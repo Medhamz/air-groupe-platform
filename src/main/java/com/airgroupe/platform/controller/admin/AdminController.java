cb92284d052106bead8e2d08cb8dc1c678b5c39e
Sidi Mohamed Hamza <smhamza@opessoftware.com>
2026-08-31 00:12:04 +0100

    Fixing newsletter 6

--- src/main/java/com/airgroupe/platform/controller/admin/AdminController.java
+++ src/main/java/com/airgroupe/platform/controller/admin/AdminController.java
@@ -84,21 +84,21 @@ public class AdminController {
         }
 
         try {
-            // Sauts de ligne convertis en balises HTML <br>
+            // Conversion des sauts de ligne en balises <br>
             String formattedContent = content.replaceAll("\n", "<br>");
 
-            // Construction du modèle HTML avec la signature
+            // Template HTML avec signature au nom de "Afrique Équipements et Services"
             String htmlBody = "<html><body style='font-family: Arial, sans-serif; color: #333; line-height: 1.6;'>"
                     + "<div>" + formattedContent + "</div>"
                     + "<br><hr style='border: none; border-top: 1px solid #ddd; margin: 25px 0;'>"
-                    + "<!-- SIGNATURE ENTERPRISE -->"
+                    + "<!-- SIGNATURE ENTREPRISE -->"
                     + "<table style='width: 100%; max-width: 550px; font-family: Arial, sans-serif;'>"
                     + "  <tr>"
                     + "    <td style='vertical-align: middle; width: 100px; padding-right: 15px;'>"
-                    + "      <img src='cid:companyLogo' alt='Air Groupe Logo' style='width: 90px; height: auto; display: block;' />"
+                    + "      <img src='cid:companyLogo' alt='Afrique Équipements et Services Logo' style='width: 90px; height: auto; display: block;' />"
                     + "    </td>"
                     + "    <td style='vertical-align: middle; border-left: 3px solid #ffc107; padding-left: 15px;'>"
-                    + "      <h3 style='margin: 0; color: #121824; font-size: 16px; font-weight: bold;'>AIR GROUPE</h3>"
+                    + "      <h3 style='margin: 0; color: #121824; font-size: 16px; font-weight: bold;'>Afrique Équipements et Services</h3>"
                     + "      <p style='margin: 3px 0; color: #555; font-size: 13px;'>Plateforme & Services Corporate</p>"
                     + "      <p style='margin: 3px 0; color: #777; font-size: 12px;'>Email: <a href='mailto:sidimohamedhamza2@gmail.com' style='color: #d4a017; text-decoration: none;'>sidimohamedhamza2@gmail.com</a></p>"
                     + "    </td>"
@@ -124,7 +124,7 @@ public class AdminController {
                 mailSender.send(message);
             }
 
-            redirectAttributes.addFlashAttribute("successMessage", "Campagne envoyée avec succès avec la signature à " + subscribers.size() + " abonné(s) !");
+            redirectAttributes.addFlashAttribute("successMessage", "Campagne envoyée avec succès à " + subscribers.size() + " abonné(s) !");
         } catch (Exception e) {
             redirectAttributes.addFlashAttribute("errorMessage", "Erreur lors de l'envoi de la newsletter : " + e.getMessage());
         }
